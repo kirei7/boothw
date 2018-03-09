6c469339272b83b972fccc81bc93f097fcbc554f
Vladyslav Sereda <Vladyslav_Sereda1@epam.com>
2018-03-09 19:09:21 +0200

    Adding form to index page

--- src/main/java/com/epam/rd/boothw/dto/AuthorDto.java
+++ src/main/java/com/epam/rd/boothw/dto/AuthorDto.java
@@ -2,8 +2,10 @@ package com.epam.rd.boothw.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class AuthorDto {
     private String name;
