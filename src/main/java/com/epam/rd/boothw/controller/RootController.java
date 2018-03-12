26625e6a4d5c7a1f254cb47f0dbac14958c0596c
Vladyslav Sereda <Vladyslav_Sereda1@epam.com>
2018-03-12 14:02:10 +0200

    Fix profiles, add logging

--- src/main/java/com/epam/rd/boothw/controller/RootController.java
+++ src/main/java/com/epam/rd/boothw/controller/RootController.java
@@ -2,6 +2,8 @@ package com.epam.rd.boothw.controller;
 
 import com.epam.rd.boothw.dto.AuthorDto;
 import com.epam.rd.boothw.service.AuthorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,8 @@ import java.util.Map;
 public class RootController {
 
     private AuthorService authorService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(RootController.class);
+
 
     @Autowired
     public RootController(AuthorService authorService) {
@@ -21,6 +25,7 @@ public class RootController {
 
     @RequestMapping
     public String welcomePage(Map<String, Object> model) {
+        LOGGER.info("Welcome page requested.");
         model.put("authors", authorService.findAll());
         model.put("newAuthor", new AuthorDto());
         return "index";
